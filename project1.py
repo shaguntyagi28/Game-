c2b3167cc4f5a6fb724e1d6888c95d904993630f
Parthav46 <parthav46@gmail.com>
2020-10-11 16:57:25 +0530

    implmented functions

--- project1.py
+++ project1.py
@@ -1,374 +1,42 @@
-print("\n\n\nWelcome to the most interesting quiz, The ShinChan Quiz\n")
-#questions with their respective options, choosing the correct option will lead you to another question.
-print('''
-Q1 - what is the full name of shinchan?
-a - suzuki Nohara
-b - Shin Chan Nohara
-c - Shinnosuke Nohara
-''')
-answer = input().lower()
+from random import random
 
-if answer == "a":
-	print(" Nope - this is not the right answer :( ")
-	print("The game has been terminated")
-elif answer == "b":
-	print(" Correct!! :), you have got the right option! Moving ahead ")
-	print('''
-Q2 -Where do the shinchan and his family live?
-a - America
-b - Korea
-c - Japan
-''')
-	answer = input().lower()
+wrong_reply = [" dont be silly! ", " Nope - this is not the right answer :( "]
 
-	if answer == "a":
-		print(" Nope - this is not the right answer :(  ")
-		print("The game has been terminated")
-	elif answer == "b":
-		print(" dont be silly ")
-		print("The game has been terminated")
-	elif answer == "c":
-			print(" correct!! :), you have got the right option! Moving ahead ")
-			print('''
-Q3 -What is the name of his pet dog?
-a - Sheero
-b - puppy
-c - michan
-''')
-			answer = input().lower()
+def ask(number, text, ans):
+    question = text.split(',')
+    if len(question) == 4:
+        print("Q" + str(number) + " - " + question[0])
+        print("a - " + question[1])
+        print("b - " + question[2])
+        print("c - " + question[3])
+        i = input("-> ").lower()
+        if int(ans) == ord(i) - 97:
+            print(" correct!! :), you have got the right option! ", end="")
+            return 1
+        else:
+            print(wrong_reply[int(random() * 2)])
+    return 0
 
-			if answer == "a":
-				print(" correct!! :), you have got the right option! Moving ahead ")
-				print('''
-Q4 -Where Shinchan's grandparents live?
-a - Town
-b - Village
-c - roadside
-''')
-				answer = input().lower()
 
-				if answer == "a":
-					print(" dont be silly`!")
-					print("The game has been terminated")
-				elif answer == "b":
-					print(" correct!! :), you have got the right option! Moving ahead ")
-					print('''
-Q5 -What is Shinchan's sister name?
-a - Hema walini
-b - Himawari Nohara
-c - Daisy
-''')
-					answer = input().lower()
+if __name__ == "__main__":
+    print("\n\n\nWelcome to the most interesting quiz, The ShinChan Quiz\n")
+    question_data = ""
+    answers = ""
+    
+    with open("question-data.csv") as f:
+        question_data = f.readlines()
 
-					if answer == "a":
-						print(" dont be silly`!")
-						print("The game has been terminated")
-					elif answer == "b":
-						print(" correct!! :), you have got the right option! Moving ahead ")
-						print('''
-Q6 -where does the shinchan live in japan?
-a - Hokaido
-b - Bejing
-c - Tokyo
-''')
-						answer = input().lower()
-
-						if answer == "a":
-							print(" dont be silly`!")
-							print("The game has been terminated")
-						elif answer == "b":
-							print(" Nope - this is not the right answer :( ")
-							print("The game has been terminated")
-						elif answer == "c":
-							print(" correct!! :), you have got the right option! Moving ahead ")
-							print('''
-Q7 -Who is Shinchan's teacher among these options?
-a - Yoshinaga maam
-b - Matsuzaka maam
-c - Principal sir
-''')
-							answer = input().lower()
-
-							if answer == "a":
-								print(" correct!! :), you have got the right option! Moving ahead ")
-								print('''
-Q8 -What does Shinchan love to eat?
-a - Dora cakes
-b - Choco chips
-c - Prawns
-''')
-								answer = input().lower()
-
-								if answer == "a":
-									print(" Nope - this is not the right answer :( ")
-									print("The game has been terminated")
-								elif answer == "b":
-									print(" correct!! :), you have got the right option! Moving ahead ")
-									print('''
-Q9 - Which is Shinchan's favourite cartoon show?
-a - Doraemon
-b - Super Fairies
-c - Action Kamen
-''')
-									answer = input().lower()
-
-									if answer == "a":
-										print(" Nope - this is not the right answer :( ")
-										print("The game has been terminated")
-									elif answer == "b":
-										print(" dont be silly`!")
-										print("The game has been terminated")
-									elif answer == "c":
-										print(" correct!! :), you have got the right option! Moving ahead ")
-										print('''
-Q10 - Who is Shinchan's best friend?
-a - Wochan
-b - Kazama
-c - Masau
-''')
-										answer = input().lower()
-									
-										if answer == "a":
-											print(" Nope - this is not the right answer :( ")
-											print("The game has been terminated")
-										elif answer == "b":
-											print(" correct!! :), you have got the right option! Moving ahead ")
-											print('''
-Q11 - what is the name of Shinchan's father?
-a - Harry
-b - kazama
-c - Bo
-''')
-											answer = input().lower()
-									
-											if answer == "a":
-												print(" correct!! :), you have got the right option! Moving ahead ")
-												print('''
-Q12 - What colour T-shirt does Shinchan usually wear?
-a - Green
-b - Red
-c - Yellow
-''')
-												answer = input().lower()
-									
-												if answer == "a":
-													print(" Nope - this is not the right answer :( ")
-													print("The game has been terminated")
-												elif answer == "b":
-													print(" correct!! :), you have got the right option! Moving ahead ")
-													print('''
-Q13 - Whom does Shinchan have a crush on?
-a - Nanako
-b - Mitsy
-c - Yoshinaga
-''')
-													answer = input().lower()
-									
-													if answer == "a":
-														print(" Nope - this is not the right answer :( ")
-														print("The game has been terminated")
-													elif answer == "b":
-														print(" dont be silly`!")
-														print("The game has been terminated")
-													elif answer == "c":
-														print(" correct!! :), you have got the right option! Moving ahead ")
-														print('''
-Q14 - What is the shin chan's gang name?
-a - Kolta mary
-b - Raging bull
-c - Kasukabe defence group
-''')
-														answer = input().lower()
-									
-														if answer == "a":
-															print(" Nope - this is not the right answer :( ")
-															print("The game has been terminated")
-														elif answer == "b":
-															print(" dont be silly`!")
-															print("The game has been terminated")
-														elif answer == "c":
-															print(" correct!! :), you have got the right option! Moving ahead ")
-															print('''
-Q15 - Who are yoshirin and michi?
-a - Shinchan's neighbours
-b - Shinchan's cousins
-c - Shinchan's teachers
-''')
-															answer = input().lower()
-									
-															if answer == "a":
-																print(" correct!! :), you have got the right option! Moving ahead ")
-																print('''
-Q16 - What is the name of Shinchan's mother?
-a - Merry
-b - Mitsy
-c - Kachin
-''')
-																answer = input().lower()
-									
-																if answer == "a":
-																	print(" dont be silly`!")
-																	print("The game has been terminated")
-							
-																elif answer == "b":
-																	print(" correct!! :), you have got the right option! Moving ahead ")
-																	print('''
-Q17 - Why did shinchan's mother beat him?
-a - because shin chan beat his sister
-b - because his mother hate him
-c - because shinchan is very nauty
-''')
-																	answer = input().lower()
-									
-																	if answer == "a":
-																		print(" dont be silly`!")
-																		print("The game has been terminated")
-							
-																	elif answer == "b":
-																		print(" Nope - this is not the right answer :( ")
-																		print("The game has been terminated")
-																	elif answer == "c":
-																		print(" correct!! :), you have got the right option! Moving ahead ")
-																		print('''
-Q18 -Shinchan is how much old?
-a - 5
-b - 4
-c - 6
-''')
-																		answer = input().lower()
-									
-																		if answer == "a":
-																			print(" correct!! :), you have got the right option! Moving ahead ")
-																			print('''
-Q19 -What vegetable does Shincan Hates the most?
-a - Spinach
-b - Only beans
-c - Capsicum and peas
-''')
-																			answer = input().lower()
-									
-																			if answer == "a":
-																				print(" dont be silly`!")
-																				print("The game has been terminated")
-							
-																			elif answer == "b":
-																				print(" Nope - this is not the right answer :( ")
-																				print("The game has been terminated")
-																			elif answer == "c":
-																				print(" correct!! :), you have got the right option!\n Congratulations you completed the game successfully! :) :)")
-																			else:
-																				print(" You didn't choose a, b or c :( ")
-																				print("The game has been terminated")
-																		elif answer == "b":
-																			print(" Nope - this is not the right answer :( ")
-																			print("The game has been terminated")
-																		elif answer == "c":
-																			print(" dont be silly`!")
-																			print("The game has been terminated")
-																		else:
-																			print(" You didn't choose a, b or c :( ")
-																			print("The game has been terminated")
-																	else:
-																		print(" You didn't choose a, b or c :( ")
-																		print("The game has been terminated")
-																elif answer == "c":
-																	print(" Nope - this is not the right answer :( ")
-																	print("The game has been terminated")
-																else:
-																	print(" You didn't choose a, b or c :( ")
-																	print("The game has been terminated")
-															elif answer == "b":
-																print(" dont be silly`!")
-																print("The game has been terminated")
-															elif answer == "c":
-																print(" Nope - this is not the right answer :( ")
-																print("The game has been terminated")
-															else:
-																print(" You didn't choose a, b or c :( ")
-																print("The game has been terminated")
-							
-							
-														else:
-															print(" You didn't choose a, b or c :( ")
-															print("The game has been terminated")
-							
-													else:
-														print(" You didn't choose a, b or c :( ")
-														print("The game has been terminated")
-												elif answer == "c":
-													print(" dont be silly`!")
-													print("The game has been terminated")
-							
-												else:
-													print(" You didn't choose a, b or c :( ")
-													print("The game has been terminated")
-											elif answer == "b":
-												print(" Nope - this is not the right answer :( ")
-												print("The game has been terminated")
-											elif answer == "c":
-												print(" dont be silly`!")
-												print("The game has been terminated")
-							
-											else:
-												print(" You didn't choose a, b or c :( ")
-												print("The game has been terminated")
-										elif answer == "c":
-											print(" dont be silly`!")
-											print("The game has been terminated")
-							
-										else:
-											print(" You didn't choose a, b or c :( ")
-											print("The game has been terminated")
-									else:
-										print(" You didn't choose a, b or c :( ")
-										print("The game has been terminated")
-								elif answer == "c":
-									print(" dont be silly`!")
-									print("The game has been terminated")
-							
-								else:
-									print(" You didn't choose a, b or c :( ")
-									print("The game has been terminated")	
-							elif answer == "b":
-								print(" Nope - this is not the right answer :( ")
-								print("The game has been terminated")
-							elif answer == "c":
-								print(" dont be silly`!")
-								print("The game has been terminated")
-							
-							else:
-								print(" You didn't choose a, b or c :( ")
-								print("The game has been terminated")	
-						else:
-							print(" You didn't choose a, b or c :( ")
-							print("The game has been terminated")	
-					elif answer == "c":
-						print(" Nope - this is not the right answer :(  ")
-						print("The game has been terminated")
-					else:
-						print(" You didn't choose a, b or c :( ")
-						print("The game has been terminated")	
-				elif answer == "c":
-					print(" Nope - this is not the right answer :(  ")
-					print("The game has been terminated")
-				else:
-					print(" You didn't choose a, b or c :( ")
-					print("The game has been terminated")
-			elif answer == "b":
-				print(" dont be silly ")
-				print("The game has been terminated")
-			elif answer == "c":
-				print(" Nope - this is not the right answer :(  ")
-				print("The game has been terminated")
-			else:
-				print(" You didn't choose a, b or c :( ")
-				print("The game has been terminated")
-	else:
-		print(" You didn't choose a, b or c :( ")
-		print("The game has been terminated")
-elif answer == "c":
-	print(" Don't be silly! :( ")
-	print("The game has been terminated")
-else:
-	print(" You didn't choose a, b or c :( ")
-	print("The game has been terminated")
+    with open("answer-data.csv") as f:
+        answers = f.readlines()
+    
+    l = len(question_data)
+    resp = 0
+    for i in range(l):
+        resp = ask(i+1, question_data[i], answers[i])
+        if resp == 0: break
+        if i < l-1: print(" Moving ahead\n")
+    
+    if resp == 1:
+        print("\nCongratulations you completed the game successfully! :) :)")
+    else:
+        print("The game has been terminated")
